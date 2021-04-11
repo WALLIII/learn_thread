f1c98abaedad252c9d8618a53c4b6ff018741431
sid-xin@live.cn <sid-xin@live.cn>
2021-04-11 10:48:26 +0200

    git_test

---
+++ build/CMakeFiles/tt.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../bin/tt"
+  "../bin/tt.pdb"
+  "CMakeFiles/tt.dir/src/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
